2f1eb72e595c6bec5003d190cfbafb42db43fe53
yicr <yicr@users.noreply.github.com>
2026-04-01 07:15:59 +0000

    chore(ci): grant workflows write to Projen GitHub App

--- .projenrc.ts
+++ .projenrc.ts
@@ -61,6 +61,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
       permissions: {
         pullRequests: github.workflows.AppPermission.WRITE,
         contents: github.workflows.AppPermission.WRITE,
+        workflows: github.workflows.AppPermission.WRITE,
       },
     }),
   },
